678d9c91f5ca83bf9a53510748deea956fd59865
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-11-24 13:11:06 +0700

    Jawaban untuk soal Angkot menggunakan perulangan while dan for
    Menghitung 1 hingga 6 menggunakan While Loop
    Menghitung 6 hingga 10 menggunakan For Loop

---
+++ SoalFor.js
@@ -0,0 +1,28 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1; 
+
+while (noAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. '  + noAngkot + ' beroperasi dengan baik.');
+    noAngkot++
+}
+
+/*
+Akan memunculkan variabel noAngkot dari 1 sampai dengan 6
+
+Angkot No. 1 beroperasi dengan baik.
+Angkot No. 6 beroperasi dengan baik.
+*/
+
+//=====//=====//=====//=====//=====//=====//=====//=====//
+
+for ( noAngkot ; noAngkot <= 10  ; noAngkot++) {
+	console.log('Angkot No. '  + noAngkot + ' beroperasi dengan tidak baik.');
+}
+
+/*
+Lalu nilai variabel terakhirnya yaitu 6 akan digunakan untuk menghitung pperulangan selanjutnya yang dimulai dari 6 hingga 10
+
+Angkot No. 6 beroperasi dengan tidak baik.
+Angkot No. 10 beroperasi dengan tidak baik.
+*/
