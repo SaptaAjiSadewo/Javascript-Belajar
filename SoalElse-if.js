4315dabae6ba2070f3490fd5661f8dc636f71e5f
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-11-24 13:36:48 +0700

    Add angkot operation status logging

---
+++ SoalElse-if.js
@@ -0,0 +1,11 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 1;
+
+for (var noAngkot = 1 ; noAngkot <= jmlAngkot ; noAngkot++){
+	
+	if (noAngkot <= 6){
+		console.log('Angkot No. '  + noAngkot + ' beroperasi dengan baik.');
+	} else {
+		console.log('Angkot No. '  + noAngkot + ' beroperasi dengan tidak baik.');
+	}
+}
