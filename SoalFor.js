8c9cab4e2c6f3343296e6f9b41fb6bad5f2626de
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-11-24 13:18:27 +0700

    Update SoalFor.js

--- SoalFor.js
+++ SoalFor.js
@@ -16,12 +16,12 @@ Angkot No. 6 beroperasi dengan baik.
 
 //=====//=====//=====//=====//=====//=====//=====//=====//
 
-for ( noAngkot ; noAngkot <= 10  ; noAngkot++) {
+for ( noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot  ; noAngkot++) {
 	console.log('Angkot No. '  + noAngkot + ' beroperasi dengan tidak baik.');
 }
 
 /*
-Lalu nilai variabel terakhirnya yaitu 6 akan digunakan untuk menghitung pperulangan selanjutnya yang dimulai dari 6 hingga 10
+Lalu nilai variabel terakhirnya yaitu 6 akan digunakan untuk menghitung perulangan selanjutnya yang dimulai dari 7 hingga 10
 
 Angkot No. 6 beroperasi dengan tidak baik.
 Angkot No. 10 beroperasi dengan tidak baik.
