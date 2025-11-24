a7f45a89408baaeaa2dafc478c87e842c23e04dc
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-11-24 13:24:03 +0700

    Implement angkot operational status logging
    Add logic to display operational status of angkots

---
+++ SoalFor-dari-saya.js
@@ -0,0 +1,15 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1; 
+
+while (noAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. '  + noAngkot + ' beroperasi dengan baik.');
+    noAngkot++
+}
+
+console.log(noAngkot); // 7 
+
+for ( noAngkot ; noAngkot <= jmlAngkot  ; noAngkot++) {
+	console.log('Angkot No. '  + noAngkot + ' beroperasi dengan tidak baik.');
+}
+
