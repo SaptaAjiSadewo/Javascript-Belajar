49965c16d280c4b6a9afac1849ff8d09c8b599fc
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-11-26 16:08:42 +0700

    Implement passenger removal functionality
    Add function to remove a passenger from the array.

--- PengelolaanPenumpang1.js
+++ PengelolaanPenumpang1.js
@@ -43,3 +43,47 @@ let tambahPenumpang = function(namaPenumpang, penumpang){
         }
     }
 }
+
+let hapusPenumpang = function(namaPenumpang, penumpang){
+
+    // jika angkot kosong
+    if(penumpang.length == 0){
+
+        // tampilkan pesan jika angkot kosong
+        console.log('Angkot masih kosong');
+
+        // kembalikan isi array dan keluar dari function
+        return penumpang;
+
+    // jika angkot ada isinya
+    } else {
+
+        for(let i = 0; i < penumpang.length; i++){
+
+            // Apakah penumpang sama dengan penumpang yang dikirim
+            if (penumpang[i] == namaPenumpang) {
+
+                // Menghapus atau mengisi penumpang itu dengan undefined
+                penumpang[i] = undefined;
+                
+                //tampilkan pesan hapusnya 
+                console.log(namaPenumpang + ' Sudah dihapus');
+
+                // kembalikan isi array dan Keluar dari function
+                return penumpang;
+            }
+
+            // kalo tidak ada penumpangnya
+            else if (penumpang[i] == undefined) {
+                /* (i == penumpang.length - 1) */
+
+                //tampilkan pesan tidak ada 
+                console.log(namaPenumpang + ' Tidak ada di dalam angkot');
+
+                // kembalikan isi array dan Keluar dari function
+                return penumpang;
+            }            
+        }
+    }
+}
+
